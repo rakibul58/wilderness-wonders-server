4cff82d5430853a6fbd34bfab78dd49cb9d1edb7
rakibul58 <rhrahi14@gmail.com>
2024-07-09 18:17:30 +0600

    refoctored

--- src/app/modules/Product/product.route.ts
+++ src/app/modules/Product/product.route.ts
@@ -1,29 +1,7 @@
 import express from 'express';
-import validateRequest from '../../middlewares/validateRequest';
-import { CarValidations } from './product.validation';
-import { CarControllers } from './product.controller';
 
 const router = express.Router();
 
-router.route('/').get(CarControllers.getAllCars).post(
-  validateRequest(CarValidations.createCarValidationSchema), // validating with zod validation
-  CarControllers.createCar,
-);
+router.route('/').get().post();
 
-router
-  .route('/return')
-  .put(
-    validateRequest(CarValidations.returnCarBookingValidationSchema),
-    CarControllers.returnCarUpdate,
-  );
-
-router
-  .route('/:id')
-  .get(CarControllers.getSingleCar)
-  .put(
-    validateRequest(CarValidations.updateCarValidationSchema),
-    CarControllers.updateSingleCar,
-  )
-  .delete(CarControllers.deleteSingleCar); // checking if the user role is user
-
-export const CarRoutes = router;
+export const ProductRoutes = router;
