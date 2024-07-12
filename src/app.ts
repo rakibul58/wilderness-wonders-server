d5a549ca2b25ccc45af7a0eb0dfb09b7c0df4878
rakibul58 <rhrahi14@gmail.com>
2024-07-12 23:40:17 +0600

    changes

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://wilderness-wonders.vercel.app'] }));
 app.use(cookieParser());
 
 //application routes
